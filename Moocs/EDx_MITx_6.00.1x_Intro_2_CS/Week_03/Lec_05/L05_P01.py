3b90377adcd57427c21ef98240497781bf9fca04
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2014-07-06 17:58:27 +0530

    Week03 finger excercise

---
+++ Moocs/EDx_MITx_6.00.1x_Intro_2_CS/Week_03/Lec_05/L05_P01.py
@@ -0,0 +1,34 @@
+''' Write an iterative function iterPower(base, exp)
+that calculates the exponential , (base ^ exp)
+by simply using successive multiplication.
+For example, iterPower(base, exp) should compute (base ^ exp) by
+multiplying base times itself exp times. 
+
+This function should take in two values - base can be a float or an integer, 
+exp will be an integer >= 0.
+It should return one numerical value. 
+Your code must be iterative - use of the ** operator
+is not allowed. 
+'''
+
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    result = base
+    if exp == 0:
+    	result = 1
+    	return result
+    elif exp > 0:
+    	while exp > 1:
+    		result *= base
+    		exp -= 1
+    	
+    return result
+
+print iterPower(4.98,4)
